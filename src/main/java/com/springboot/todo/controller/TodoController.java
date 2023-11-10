873be8fb11d6849fa3bb1a1bbd2d4dfdcc740b64
Dharun-Srikanth <dharun.srikanth@kumaran.com>
2023-11-10 09:16:15 +0530

    updated

--- src/main/java/com/springboot/todo/controller/TodoController.java
+++ src/main/java/com/springboot/todo/controller/TodoController.java
@@ -9,6 +9,7 @@ import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/todo/api")
 public class TodoController {
