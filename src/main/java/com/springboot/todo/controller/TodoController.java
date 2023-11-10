ed4a39f37ac7b252af359431a3dc7e3390387888
Dharun-Srikanth <dharun.srikanth@kumaran.com>
2023-11-10 09:19:06 +0530

    updated

--- src/main/java/com/springboot/todo/controller/TodoController.java
+++ src/main/java/com/springboot/todo/controller/TodoController.java
@@ -5,8 +5,6 @@ import com.springboot.todo.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin
